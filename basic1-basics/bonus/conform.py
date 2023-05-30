7b5650032ab0672596be34683b94eb348589ea06
longieee <long.nguyen-duy@outlook.com>
2023-05-30 21:34:25 +0700

    add skeleton

---
+++ basic1-basics/bonus/conform.py
@@ -0,0 +1,18 @@
+"""
+Input is a vector of F's and B's, in terms of forwards and backwards caps
+Output is a set of commands (printed out) to get either all F's or all B's
+Fewest commands are the goal!
+
+Please see the problem write-up to get more details
+"""
+
+caps = ["F", "F", "B", "B", "B", "F", "B", "B", "B", "F", "F", "B", "F"]
+cap2 = ["F", "F", "B", "B", "B", "F", "B", "B", "B", "F", "F", "F", "F"]
+
+
+def pleaseConform(caps):
+    pass
+
+
+pleaseConform(caps)
+##pleaseConform(cap2)
