452bc56623f2f4fcb63c2caa6ff0857bfc864a32
longieee <long.nguyen-duy@outlook.com>
2023-06-12 21:12:07 +0700

    add ml1 exercises

---
+++ ml1-intro/exercise/README.md
@@ -0,0 +1,7 @@
+# Introduction to machine learning exercies
+
+## Linear regression with gradient descend
+
+Implement the Linear regression model to predict, but rather than using the statistical methods, you will need to implement it with the Gradient Descend algorithm
+
+Coding-wise, the exercise it not difficult. The challenge in the exercise lies within the understanding of the model and the algorithm
---
+++ ml1-intro/exercise/linear_regression_gd/lr_gd.py
@@ -0,0 +1,35 @@
+"""Linear regression model for ML regression task, but implemented with the gradient descend algorithm
+Name:
+Colaborator:
+Time:
+"""
+# import
+
+# ---------------------------------------------------------------------------- #
+#                                Best practices                                #
+# ---------------------------------------------------------------------------- #
+# Remember to code with FUNCTION DOCSTRINGS
+# Also, whenever possible, in the function signature you should
+# put in TYPE HINT. Example
+# def some_func(arg1:int, arg2:float=0, arg3:bool=False) -> float:
+#       ...do something ...
+
+
+class LinearRegression:
+    def __init__(self):
+        pass
+
+    def y_hat(self, X, w):
+        pass
+
+    def cost(self, yhat, y):
+        pass
+
+    def gradient_descent(self, w, X, y, yhat):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
